d4990100e9b0654755eac03443d733d0b44663f1
Mathias Hall-Andersen <mathias@hall-andersen.dk>
2022-08-06 13:35:44 +0200

    Secret lookup in 3-bit table

---
+++ relations/src/lib.rs
@@ -0,0 +1,14 @@
+// 3-bit lookups
+mod lookup;
+
+// curve operations
+mod curve;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
