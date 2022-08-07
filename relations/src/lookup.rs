c61b5079efc3562c09a5bfe9d925f63a22c59030
Mathias Hall-Andersen <mathias@hall-andersen.dk>
2022-08-07 17:11:08 +0200

    Work on relations

--- relations/src/lookup.rs
+++ relations/src/lookup.rs
@@ -1,28 +1,42 @@
 use bulletproofs::r1cs::*;
 
-use ark_ff::{Field, One, Zero};
 use ark_ec::AffineCurve;
+use ark_ff::{Field, One, Zero};
 
 const WINDOW_SIZE: usize = 3;
 const WINDOW_ELEMS: usize = 1 << WINDOW_SIZE;
 
 pub struct Lookup3Bit<const N: usize, F: Field> {
-    elems: [[F; WINDOW_ELEMS]; N]
+    elems: [[F; WINDOW_ELEMS]; N],
 }
 
 fn b2f<F: Field>(v: bool) -> F {
-    if v { F::one() } else { F::zero() }
+    if v {
+        F::one()
+    } else {
+        F::zero()
+    }
 }
 
-fn is_bit<C: AffineCurve, Cs: ConstraintSystem<C>>(cs: &mut Cs, var: LinearCombination<C::ScalarField>) {
+fn is_bit<C: AffineCurve, Cs: ConstraintSystem<C>>(
+    cs: &mut Cs,
+    var: LinearCombination<C::ScalarField>,
+) {
     let (_, _, zero) = cs.multiply(var.clone(), var - C::ScalarField::one());
     cs.constrain(zero.into());
 }
 
-fn bit<C: AffineCurve, Cs: ConstraintSystem<C>>(cs: &mut Cs, val: Option<bool>) -> Result<Variable<C::ScalarField>, R1CSError> {
+fn bit<C: AffineCurve, Cs: ConstraintSystem<C>>(
+    cs: &mut Cs,
+    val: Option<bool>,
+) -> Result<Variable<C::ScalarField>, R1CSError> {
     // compute assignment (if witness is provided)
     let assignment = val.map(|b| {
-        let s = if b { C::ScalarField::one() } else { C::ScalarField::zero() };
+        let s = if b {
+            C::ScalarField::one()
+        } else {
+            C::ScalarField::zero()
+        };
         (s, s - C::ScalarField::one())
     });
 
@@ -39,32 +53,44 @@ fn bit<C: AffineCurve, Cs: ConstraintSystem<C>>(cs: &mut Cs, val: Option<bool>) 
 }
 
 fn single_membership<C: AffineCurve, Cs: ConstraintSystem<C>>(
-    cs: &mut Cs, 
+    cs: &mut Cs,
     u: &[C::ScalarField; WINDOW_ELEMS],
     sa: LinearCombination<C::ScalarField>, // product
     s0: LinearCombination<C::ScalarField>, // bit
     s1: LinearCombination<C::ScalarField>, // bit
     s2: LinearCombination<C::ScalarField>, // bit
 ) -> LinearCombination<C::ScalarField> {
-
     // left side
-    let (_, _, left): (Variable<C::ScalarField>, Variable<C::ScalarField>, Variable<C::ScalarField>) = cs.multiply(s0.clone(), {
-        let f = -(sa.clone() * u[0]) + (s2.clone() * u[0]) + (s1.clone() * u[0]) - u[0] + (sa.clone() * u[2]);
-        let f = f - (s1.clone() * u[2]) + (sa.clone() * u[4]) - (s2.clone() * u[4]) - (sa.clone() * u[6]);
-        let f = f + (sa.clone() * u[1]) - (s2.clone() * u[1]) - (s1.clone() * u[1]) + u[1] - (sa.clone() * u[3]);
-        let f = f + (s1.clone() * u[3]) - (sa.clone() * u[5]) + (s2.clone() * u[5]) + (sa.clone() * u[7]);
+    let (_, _, left): (
+        Variable<C::ScalarField>,
+        Variable<C::ScalarField>,
+        Variable<C::ScalarField>,
+    ) = cs.multiply(s0.clone(), {
+        let f = -(sa.clone() * u[0]) + (s2.clone() * u[0]) + (s1.clone() * u[0]) - u[0]
+            + (sa.clone() * u[2]);
+        let f = f - (s1.clone() * u[2]) + (sa.clone() * u[4])
+            - (s2.clone() * u[4])
+            - (sa.clone() * u[6]);
+        let f = f + (sa.clone() * u[1]) - (s2.clone() * u[1]) - (s1.clone() * u[1]) + u[1]
+            - (sa.clone() * u[3]);
+        let f = f + (s1.clone() * u[3]) - (sa.clone() * u[5])
+            + (s2.clone() * u[5])
+            + (sa.clone() * u[7]);
         f
     });
 
     // right size
-    let right = - (sa.clone() * u[0]) + (s2.clone() * u[0]) + (s1.clone() * u[0]) - u[0] + (sa.clone() * u[2]);
-    let right = right - (s1.clone() * u[2]) + (sa.clone() * u[4]) - (s2.clone() * u[4]) - (sa.clone() * u[6]);
+    let right = -(sa.clone() * u[0]) + (s2.clone() * u[0]) + (s1.clone() * u[0]) - u[0]
+        + (sa.clone() * u[2]);
+    let right = right - (s1.clone() * u[2]) + (sa.clone() * u[4])
+        - (s2.clone() * u[4])
+        - (sa.clone() * u[6]);
 
     // sum is the element
     left - right
 }
 
-impl <const N: usize, F: Field> Lookup3Bit<N, F> {
+impl<const N: usize, F: Field> Lookup3Bit<N, F> {
     fn lookup(&self, index: usize) -> [F; N] {
         assert!(index < WINDOW_ELEMS);
         let val: Vec<_> = (0..N).map(|i| self.elems[i][index].clone()).collect();
@@ -74,21 +100,13 @@ impl <const N: usize, F: Field> Lookup3Bit<N, F> {
 
 // The witneness (provided when proving/None when verifying) is the secret index
 pub fn lookup<const N: usize, C: AffineCurve, Cs: ConstraintSystem<C>>(
-    cs: &mut Cs, 
+    cs: &mut Cs,
     table: &Lookup3Bit<N, C::ScalarField>,
-    index: Option<usize>
+    index: Option<usize>,
 ) -> Result<[LinearCombination<C::ScalarField>; N], R1CSError> {
-   
     // compute multiplication of higher bits
-    let (b1, b2, ba) = cs.allocate_multiplier(
-        index.map(|i| {
-            (
-                b2f((i >> 1) & 1 == 1),
-                b2f((i >> 2) & 1 == 1),
-            )
-        })
-    )?;
-
+    let (b1, b2, ba) =
+        cs.allocate_multiplier(index.map(|i| (b2f((i >> 1) & 1 == 1), b2f((i >> 2) & 1 == 1))))?;
 
     // enforce bits
     let b0 = bit::<C, Cs>(cs, index.map(|i| (i & 1) == 1))?;
@@ -98,21 +116,20 @@ pub fn lookup<const N: usize, C: AffineCurve, Cs: ConstraintSystem<C>>(
     // enforce membership
     let mut res: Vec<LinearCombination<_>> = Vec::with_capacity(N);
     for i in 0..N {
-        res.push(
-            single_membership(
-                cs, 
-                &table.elems[i], 
-                ba.into(), 
-                b0.into(), 
-                b1.into(), 
-                b2.into()
-            )
-        );
+        res.push(single_membership(
+            cs,
+            &table.elems[i],
+            ba.into(),
+            b0.into(),
+            b1.into(),
+            b2.into(),
+        ));
     }
-    
+
     Ok(res.try_into().unwrap())
 }
 
+#[cfg(test)]
 mod tests {
     use super::*;
 
@@ -122,11 +139,13 @@ mod tests {
     use rand::thread_rng;
     use rand::Rng;
 
+    use pasta;
+
     /*
     #[test]
     fn test_lookup() {
 
-        
+
         let pc_gens = PedersenGens::default();
         let bp_gens = BulletproofGens::new(128, 1);
 
