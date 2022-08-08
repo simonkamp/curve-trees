49903cd95a0ef6e64353f3710fa7a6cd8d46f135
Simon Kamp <simonkamp@live.dk>
2022-08-08 10:50:06 +0200

    checked curve addition

--- relations/src/curve.rs
+++ relations/src/curve.rs
@@ -1,6 +1,7 @@
 use bulletproofs::r1cs::*;
 
 use ark_ec::AffineCurve;
+use std::marker::PhantomData;
 
 fn curve_check<C: AffineCurve, Cs: ConstraintSystem<C>>(
     cs: &mut Cs,
@@ -22,9 +23,11 @@ fn curve_check<C: AffineCurve, Cs: ConstraintSystem<C>>(
     )
 }
 
-// todo pass points as structs (or single struct with delta?) with conversion from the other curve
+/// Enforce points over C::ScalarField: (x_3, y_3) = (x_1, y_1) + (x_2, y_2)
+/// Takes the slope (delta) as input
 fn incomplete_curve_addition<C: AffineCurve, Cs: ConstraintSystem<C>>(
     cs: &mut Cs,
+    // todo pass points as structs (or single struct with delta?) with conversion from the other curve
     x1: LinearCombination<C::ScalarField>,
     y1: LinearCombination<C::ScalarField>,
     x2: LinearCombination<C::ScalarField>,
@@ -44,4 +47,36 @@ fn incomplete_curve_addition<C: AffineCurve, Cs: ConstraintSystem<C>>(
     // delta * delta = x_3 + x_2 + x_1
     let (_, _, delta2) = cs.multiply(delta.clone(), delta.clone());
     cs.constrain(x3 + x2 + x1 - LinearCombination::<C::ScalarField>::from(delta2));
+}
+
+/// Enforce ()
+fn checked_curve_addition<C: AffineCurve, Cs: ConstraintSystem<C>>(
+    cs: &mut Cs,
+    x1: LinearCombination<C::ScalarField>,
+    y1: LinearCombination<C::ScalarField>,
+    x2: LinearCombination<C::ScalarField>,
+    y2: LinearCombination<C::ScalarField>,
+    x3: LinearCombination<C::ScalarField>,
+    y3: LinearCombination<C::ScalarField>,
+    delta: LinearCombination<C::ScalarField>, // free variable
+    x1_minus_x2_inv: LinearCombination<C::ScalarField>,
+) {
+    not_zero(cs, x1.clone() - x2.clone(), x1_minus_x2_inv);
+    incomplete_curve_addition(cs, x1, y1, x2, y2, x3, y3, delta);
+}
+
+// todo why are other implementations much more complex? Am I missing something?
+/// Enforce v != 0
+/// Takes v and its modular inverse (v_inv) as input
+fn not_zero<C: AffineCurve, Cs: ConstraintSystem<C>>(
+    cs: &mut Cs,
+    v: LinearCombination<C::ScalarField>,
+    v_inv: LinearCombination<C::ScalarField>,
+) {
+    // v * v_inv = one
+    let (_, _, one) = cs.multiply(v, v_inv);
+    // one = 1
+    cs.constrain(LinearCombination::<C::ScalarField>::from(
+        one - Variable::One(PhantomData),
+    ));
 }
