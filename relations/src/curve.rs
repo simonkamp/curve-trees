03faeb484f3e206829a8df44ee0b88f296a84373
Simon Kamp <simonkamp@live.dk>
2022-08-09 18:01:18 +0200

    rerandomize

--- relations/src/curve.rs
+++ relations/src/curve.rs
@@ -21,55 +21,57 @@ pub fn curve_check<F: Field, Cs: ConstraintSystem<F>>(
     a: F, // 0 for pallas and vesta
     b: F, // 5 for pallas and vesta
 ) {
-    let (_, _, x2) = cs.multiply(x.clone(), x.clone());
-    let (_, _, x3) = cs.multiply(x, x2.into());
-    let (_, _, y2) = cs.multiply(y.clone(), y);
+    let (_, _, x_r) = cs.multiply(x.clone(), x.clone());
+    let (_, _, x_o) = cs.multiply(x, x_r.into());
+    let (_, _, y_r) = cs.multiply(y.clone(), y);
 
     // x^3 + A*x^2 + B - y^2 = 0
     cs.constrain(
-        LinearCombination::<F>::from(x3) + LinearCombination::<F>::from(x2).scalar_mul(a) + b - y2,
+        LinearCombination::<F>::from(x_o) + LinearCombination::<F>::from(x_r).scalar_mul(a) + b - y_r,
     )
 }
 
 #[derive(Clone, Debug)]
-pub struct curve_addition_prms<F: Field> {
-    x1: LinearCombination<F>,
-    y1: LinearCombination<F>,
-    x2: LinearCombination<F>,
-    y2: LinearCombination<F>,
-    x3: LinearCombination<F>,
-    y3: LinearCombination<F>,
-    delta: LinearCombination<F>,
+pub struct CurveAddition<F: Field> {
+    x_l: LinearCombination<F>,
+    y_l: LinearCombination<F>,
+    x_r: LinearCombination<F>,
+    y_r: LinearCombination<F>,
+    x_o: LinearCombination<F>,
+    y_o: LinearCombination<F>,
+    delta: Option<F>,
 }
 
-/// Enforce points over C::ScalarField: (x_3, y_3) = (x_1, y_1) + (x_2, y_2)
+/// Enforce points over C::ScalarField: (x_o, y_o) = (x_l, y_l) + (x_r, y_r)
 /// Takes the slope (delta) as input
 pub fn incomplete_curve_addition<F: Field, Cs: ConstraintSystem<F>>(
     cs: &mut Cs,
-    prms: &curve_addition_prms<F>,
+    prms: &CurveAddition<F>,
 ) {
-    // delta * (x_2 - x_1) = y_2 - y_1
-    let (_, _, delta_x2_x1) = cs.multiply(prms.delta.clone(), prms.x2.clone() - prms.x1.clone()); // todo make borrow parameters?
-    cs.constrain(LinearCombination::<F>::from(delta_x2_x1) - (prms.y2.clone() - prms.y1.clone()));
+    let delta_lc: LinearCombination<F> = cs.allocate(prms.delta).unwrap().into();
+    // delta * (x_r - x_l) = y_r - y_l
+    let (_, _, delta_x_r_x_l) = cs.multiply(delta_lc.clone(), prms.x_r.clone() - prms.x_l.clone()); // todo make borrow parameters?
+    cs.constrain(LinearCombination::<F>::from(delta_x_r_x_l) - (prms.y_r.clone() - prms.y_l.clone()));
 
-    // delta * (x_3 - x_1) = - y_3 - y_1
-    let (_, _, delta_x3_x1) = cs.multiply(prms.delta.clone(), prms.x3.clone() - prms.x1.clone());
-    cs.constrain(LinearCombination::<F>::from(delta_x3_x1) - (-prms.y3.clone() - prms.y1.clone()));
+    // delta * (x_o - x_l) = - y_o - y_l
+    let (_, _, delta_x_o_x_l) = cs.multiply(delta_lc.clone(), prms.x_o.clone() - prms.x_l.clone());
+    cs.constrain(LinearCombination::<F>::from(delta_x_o_x_l) - (-prms.y_o.clone() - prms.y_l.clone()));
 
-    // delta * delta = x_3 + x_2 + x_1
-    let (_, _, delta2) = cs.multiply(prms.delta.clone(), prms.delta.clone());
+    // delta * delta = x_o + x_r + x_l
+    let (_, _, delta2) = cs.multiply(delta_lc.clone(), delta_lc.clone());
     cs.constrain(
-        prms.x3.clone() + prms.x2.clone() + prms.x1.clone() - LinearCombination::<F>::from(delta2),
+        prms.x_o.clone() + prms.x_r.clone() + prms.x_l.clone() - LinearCombination::<F>::from(delta2),
     );
 }
 
 /// Enforce ()
 pub fn checked_curve_addition<F: Field, Cs: ConstraintSystem<F>>(
     cs: &mut Cs,
-    prms: &curve_addition_prms<F>,
-    x1_minus_x2_inv: LinearCombination<F>,
+    prms: &CurveAddition<F>,
+    x_l_minus_x_r_inv: Option<F>,
 ) {
-    not_zero(cs, prms.x1.clone() - prms.x2.clone(), x1_minus_x2_inv);
+    let x_l_minus_x_r_inv_lc: LinearCombination<F> = cs.allocate(x_l_minus_x_r_inv).unwrap().into();
+    not_zero(cs, prms.x_l.clone() - prms.x_r.clone(), x_l_minus_x_r_inv_lc);
     incomplete_curve_addition(cs, prms);
 }
 
@@ -88,11 +90,12 @@ fn not_zero<F: Field, Cs: ConstraintSystem<F>>(
 
 pub fn re_randomize<F: Field, C: SWModelParameters<BaseField = F>, Cs: ConstraintSystem<F>>(
     cs: &mut Cs,
-    h: GroupAffine<C>,
-    commitment_x: F,
-    commitment_y: F,
-    commitment_x_tilde: F,
-    commitment_y_tilde: F,
+    h: GroupAffine<C>, // Constant
+    commitment_x: LinearCombination<F>,
+    commitment_y: LinearCombination<F>,
+    commitment_x_tilde: LinearCombination<F>,
+    commitment_y_tilde: LinearCombination<F>,
+    commitment: Option<GroupAffine<C>>, // Witness provided by the prover
     randomness: Option<C::ScalarField>, // Witness provided by the prover
 ) {
     let lambda = <C::ScalarField as PrimeField>::size_in_bits();
@@ -106,8 +109,11 @@ pub fn re_randomize<F: Field, C: SWModelParameters<BaseField = F>, Cs: Constrain
         }
     };
 
-    // Define tables T_1 .. T_m
+    let mut blinding_accumulator = GroupAffine::<C>::zero();
     let mut m_th_other_term = C::ScalarField::zero();
+    let mut acc_i_minus_1_x_lc: LinearCombination<F> = Variable::One(PhantomData).into();
+    let mut acc_i_minus_1_y_lc: LinearCombination<F> = Variable::One(PhantomData).into();
+    // Define tables T_1 .. T_m, and witnesses
     for i in 1..m + 1 {
         let mut table = Lookup3Bit::<2, F> {
             elems: [[F::one(); WINDOW_ELEMS]; 2],
@@ -132,8 +138,8 @@ pub fn re_randomize<F: Field, C: SWModelParameters<BaseField = F>, Cs: Constrain
             table.elems[1][j] = hs.y;
         }
 
-        let index = match &r_bits {
-            None => None,
+        let (index, x_l_minus_x_r_inv, delta, acc_i_x, acc_i_y) = match &r_bits {
+            None => (None, None, None, None, None),
             Some(random_bits) => {
                 let bi = (i - 1) * 3;
                 let mut index = if random_bits[bi] { 1usize } else { 0 };
@@ -143,24 +149,78 @@ pub fn re_randomize<F: Field, C: SWModelParameters<BaseField = F>, Cs: Constrain
                 if random_bits[bi + 2] {
                     index += 4
                 };
-                Some(index)
+                let x_i_lookup = table.elems[0][index];
+                let y_i_lookup = table.elems[1][index];
+                let x_left = blinding_accumulator.x; // read before updating blinding accumulator
+                let y_left = blinding_accumulator.y; // read before updating blinding accumulator
+                let x_right = x_i_lookup;
+                let y_right = y_i_lookup;
+                // compute slope delta
+                let delta = Some((y_right - y_left)/(x_right - x_left));
+                let x_left_minus_x_right_inv = if i == m {
+                    // compute x_l-x_r inverse for checked addition
+                    Some(F::one()/(x_left-x_right))
+                } else {
+                    None
+                };
+                blinding_accumulator = blinding_accumulator + GroupAffine::<C>::new(x_i_lookup, y_i_lookup, false);
+
+                (Some(index), x_left_minus_x_right_inv, delta, Some(blinding_accumulator.x), Some(blinding_accumulator.y))
             }
         };
 
         let [x_table, y_table] = lookup(cs, &table, index).unwrap();
-    }
-}
 
-pub fn prove_addition<
-    T: BorrowMut<Transcript>,
-    C: AffineCurve,
-    C2: SWModelParameters<BaseField = C::ScalarField>,
->(
-    prover: &mut Prover<T, C>,
-    p: C2,
-    q: C2,
-) {
-    prover.transcript().append_message(b"", b"");
+        // Allocate coordinated for the accumulated witness
+        let acc_i_x_lc: LinearCombination<F> = cs.allocate(acc_i_x).unwrap().into();
+        let acc_i_y_lc: LinearCombination<F> = cs.allocate(acc_i_y).unwrap().into();
+        if i > 1 {
+            // Enforce addition constraint:
+            // R_i = R_i-1 + (x_i, y_i)
+            let prms = CurveAddition {
+                x_l: acc_i_minus_1_x_lc.clone(),
+                y_l: acc_i_minus_1_y_lc.clone(),
+                x_r: x_table,
+                y_r: y_table,
+                x_o: acc_i_x_lc.clone(),
+                y_o: acc_i_y_lc.clone(),
+                delta: delta,
+            };
+            if i == m {
+                // enforce checked curve addition
+                checked_curve_addition(cs, &prms, x_l_minus_x_r_inv);
+            } else {
+                // enforce incomplete curve addition
+                incomplete_curve_addition(cs, &prms);
+            }
+        }
+        acc_i_minus_1_x_lc = acc_i_x_lc;
+        acc_i_minus_1_y_lc = acc_i_y_lc;
+    }
+
+    // constrain (x_tilde, y_tilde) = (x, y) + (R_m) - with checked addition
+    let (delta, x_l_minus_x_r_inv) = match (commitment) {
+        Some(commitment) => {
+            let x_left = commitment.x;
+            let y_left = commitment.y;
+            let x_right = blinding_accumulator.x;
+            let y_right = blinding_accumulator.y;
+            let delta = (y_right - y_left)/(x_right - x_left);
+            (Some(delta), Some(F::one()/(x_left-x_right)))// todo
+
+        },
+        _ => (None, None),
+    };
+    let prms = CurveAddition {
+        x_l: commitment_x,
+        y_l: commitment_y,
+        x_r: acc_i_minus_1_x_lc,
+        y_r: acc_i_minus_1_y_lc,
+        x_o: commitment_x_tilde,
+        y_o: commitment_y_tilde,
+        delta: delta, // todo
+    };
+    checked_curve_addition(cs, &prms, x_l_minus_x_r_inv);
 }
 
 #[cfg(test)]
@@ -181,81 +241,75 @@ mod tests {
         let mut rng = rand::thread_rng();
         let p = <PallasP as UniformRand>::rand(&mut rng);
         let pa = p.into_affine();
-        let x1: VestaScalar = (pa as PallasA).x;
-        let y1: VestaScalar = (pa as PallasA).y;
+        let x_l: VestaScalar = (pa as PallasA).x;
+        let y_l: VestaScalar = (pa as PallasA).y;
 
         let q = <PallasP as UniformRand>::rand(&mut rng);
         let qa = q.into_affine();
-        let x2: VestaScalar = (qa as PallasA).x;
-        let y2: VestaScalar = (qa as PallasA).y;
+        let x_r: VestaScalar = (qa as PallasA).x;
+        let y_r: VestaScalar = (qa as PallasA).y;
 
-        assert!(x1 != x2); // sanity check, should not happen except with negl. prob.
+        assert!(x_l != x_r); // sanity check, should not happen except with negl. prob.
 
         let p_plus_q = p + q;
         let p_plus_qa = p_plus_q.into_affine();
-        let x3: VestaScalar = (p_plus_qa as PallasA).x;
-        let y3: VestaScalar = (p_plus_qa as PallasA).y;
+        let x_o: VestaScalar = (p_plus_qa as PallasA).x;
+        let y_o: VestaScalar = (p_plus_qa as PallasA).y;
 
         // sanity checks
-        let delta = (y2 - y1) / (x2 - x1);
-        assert_eq!(delta * (x2 - x1), y2 - y1);
-        assert_eq!(delta * (x3 - x1), -y3 - y1);
-        assert_eq!(delta * delta, x1 + x2 + x3);
+        let delta = (y_r - y_l) / (x_r - x_l);
+        assert_eq!(delta * (x_r - x_l), y_r - y_l);
+        assert_eq!(delta * (x_o - x_l), -y_o - y_l);
+        assert_eq!(delta * delta, x_l + x_r + x_o);
 
         let pc_gens = PedersenGens::<VestaA>::default();
         let bp_gens = BulletproofGens::<VestaA>::new(128, 1); // todo size
 
         let mut transcript = Transcript::new(b"CurveAdditionGadget");
         let mut prover = Prover::new(&pc_gens, &mut transcript);
-        let (x1_comm, x1_var) = prover.commit(x1, VestaScalar::rand(&mut rng));
-        let (y1_comm, y1_var) = prover.commit(y1, VestaScalar::rand(&mut rng));
-        let (x2_comm, x2_var) = prover.commit(x2, VestaScalar::rand(&mut rng));
-        let (y2_comm, y2_var) = prover.commit(y2, VestaScalar::rand(&mut rng));
-        let (x3_comm, x3_var) = prover.commit(x3, VestaScalar::rand(&mut rng));
-        let (y3_comm, y3_var) = prover.commit(y3, VestaScalar::rand(&mut rng));
-        let (delta_comm, delta_var) = prover.commit(delta, VestaScalar::rand(&mut rng));
-        let (x1_minus_x2_inv_comm, x1_minus_x2_inv_var) = prover.commit(
-            VestaScalar::from(1) / (x1 - x2),
-            VestaScalar::rand(&mut rng),
-        );
+        let (x_l_comm, x_l_var) = prover.commit(x_l, VestaScalar::rand(&mut rng));
+        let (y_l_comm, y_l_var) = prover.commit(y_l, VestaScalar::rand(&mut rng));
+        let (x_r_comm, x_r_var) = prover.commit(x_r, VestaScalar::rand(&mut rng));
+        let (y_r_comm, y_r_var) = prover.commit(y_r, VestaScalar::rand(&mut rng));
+        let (x_o_comm, x_o_var) = prover.commit(x_o, VestaScalar::rand(&mut rng));
+        let (y_o_comm, y_o_var) = prover.commit(y_o, VestaScalar::rand(&mut rng));
+        let x_l_minus_x_r_inv = VestaScalar::from(1) / (x_l - x_r);
 
-        let add_prms = curve_addition_prms {
-            x1: x1_var.into(),
-            y1: y1_var.into(),
-            x2: x2_var.into(),
-            y2: y2_var.into(),
-            x3: x3_var.into(),
-            y3: y3_var.into(),
-            delta: delta_var.into(),
+        let add_prms = CurveAddition {
+            x_l: x_l_var.into(),
+            y_l: y_l_var.into(),
+            x_r: x_r_var.into(),
+            y_r: y_r_var.into(),
+            x_o: x_o_var.into(),
+            y_o: y_o_var.into(),
+            delta: Some(delta),
         };
 
-        checked_curve_addition(&mut prover, &add_prms, x1_minus_x2_inv_var.into());
+        checked_curve_addition(&mut prover, &add_prms, Some(x_l_minus_x_r_inv));
 
         let proof = prover.prove(&bp_gens).unwrap();
 
         let mut transcript = Transcript::new(b"CurveAdditionGadget");
         let mut verifier = Verifier::new(&mut transcript);
 
-        let x1_var = verifier.commit(x1_comm);
-        let y1_var = verifier.commit(y1_comm);
-        let x2_var = verifier.commit(x2_comm);
-        let y2_var = verifier.commit(y2_comm);
-        let x3_var = verifier.commit(x3_comm);
-        let y3_var = verifier.commit(y3_comm);
-        let delta_var = verifier.commit(delta_comm);
-        let x1_minus_x2_inv_var = verifier.commit(x1_minus_x2_inv_comm);
+        let x_l_var = verifier.commit(x_l_comm);
+        let y_l_var = verifier.commit(y_l_comm);
+        let x_r_var = verifier.commit(x_r_comm);
+        let y_r_var = verifier.commit(y_r_comm);
+        let x_o_var = verifier.commit(x_o_comm);
+        let y_o_var = verifier.commit(y_o_comm);
 
-        let add_prms = curve_addition_prms {
-            x1: x1_var.into(),
-            y1: y1_var.into(),
-            x2: x2_var.into(),
-            y2: y2_var.into(),
-            x3: x3_var.into(),
-            y3: y3_var.into(),
-            delta: delta_var.into(),
+        let add_prms = CurveAddition {
+            x_l: x_l_var.into(),
+            y_l: y_l_var.into(),
+            x_r: x_r_var.into(),
+            y_r: y_r_var.into(),
+            x_o: x_o_var.into(),
+            y_o: y_o_var.into(),
+            delta: None,
         };
 
-        checked_curve_addition(&mut verifier, &add_prms, x1_minus_x2_inv_var.into());
+        checked_curve_addition(&mut verifier, &add_prms, None);
 
         verifier.verify(&proof, &pc_gens, &bp_gens).unwrap();
     }
