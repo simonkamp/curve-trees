5408af8ea64f8ee784052a37d37c6330a49b3e8a
Matteo Campanelli <matteo.campanelli@gmail.com>
2023-07-10 14:34:01 +0200

    Update bash script for formatting tables

--- fmt_estimates.sh
+++ fmt_estimates.sh
@@ -1,12 +1,54 @@
 #!/bin/bash
 
+
 function estimate_info() {
     WHOLE_NAME="$1"
     DIR_NAME=$(dirname "$1")
-    MEAN=$(jq '.mean.point_estimate' $WHOLE_NAME | awk '{printf "%.2f", $1/1000000;}')
-    echo "$DIR_NAME $MEAN"
-    }
+    MEAN=$(jq '.mean.point_estimate' "$WHOLE_NAME" | awk '{printf "%.2f", $1/1000000;}')
+
+    # Division for batch
+    if echo "$DIR_NAME" | grep -q "100"; then
+        MEAN=$(echo "scale=2; $MEAN / 100" | bc)
+    fi
+
+
+    if echo "$DIR_NAME" | grep -q "base"; then
+        FMTTED_DIR=$(echo -n "$DIR_NAME" | cut -d / -f 4-5)
+        if echo "$DIR_NAME" | grep -q -i "$2"; then
+            printf "%-80s %s\n" "${FMTTED_DIR}" "${MEAN}ms"
+        fi
+    fi
+}
 
 export -f estimate_info
 
-find . -type f -name "estimates.json" -exec bash -c "estimate_info \"{}\"" \;
+function estimate_info_acc() {
+    estimate_info "$1" "acc"
+}
+export -f estimate_info_acc
+
+function estimate_info_selrand() {
+    estimate_info "$1" "selrand "
+}
+export -f estimate_info_selrand
+
+function estimate_info_pour() {
+    estimate_info "$1" "pour"
+}
+export -f estimate_info_pour
+
+# Main code
+echo
+
+echo "--- Table 1 (Accumulator)----"
+find . -type f -name "estimates.json" -exec bash -c "estimate_info_acc \"{}\"" \;
+echo 
+
+echo "--- Table 2 (SelectAndRerand)----"
+find . -type f -name "estimates.json" -exec bash -c "estimate_info_selrand \"{}\"" \;
+echo
+
+echo "--- Table 3 (Pour)----"
+find . -type f -name "estimates.json" -exec bash -c "estimate_info_pour \"{}\"" \;
+echo
+
